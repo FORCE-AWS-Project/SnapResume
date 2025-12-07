b9f5e20a3c8e2a746a2a82238c081f9265fc0e29
DaShork <nhatminhvo2311@gmail.com>
2025-12-07 12:16:26 +0700

    remove src folder for restructuring

--- src/components/ResumeManager/index.js
+++
@@ -1,9 +0,0 @@
-export { default as ResumeManagerModal } from './ResumeManagerModal'
-export { default as SectionsList } from './SectionsList'
-export { default as ResumePreviewPanel } from './ResumePreviewPanel'
-export {
-  useResumeManager,
-  getSectionInfo,
-  isResumeComplete,
-  getResumeCompletionPercentage
-} from './hooks'
